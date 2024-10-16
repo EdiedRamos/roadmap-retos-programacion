89c944cc83047d150b7ea10722895c2cde8d432d
EdiedRamos <edieisa22@gmail.com>
2024-10-15 23:03:49 -0500

    feat: create base models

---
+++ Roadmap/42 - TORNEO DRAGON BALL/typescript/EdiedRamos.ts
@@ -0,0 +1,176 @@
+// ======================
+// = AUTHOR: EdiedRamos =
+// ======================
+
+// ===============
+// = DEFINITIONS =
+// ===============
+
+const GENRES = ["male", "female"] as const;
+type Genre = (typeof GENRES)[number];
+
+interface Data {
+  avatars: Record<Genre, Array<string>>;
+  names: Record<Genre, Array<string>>;
+}
+
+// ===========
+// = CLASSES =
+// ===========
+
+const DATA: Data = {
+  // YOUR CUSTOM AVATARS GO HERE
+  avatars: {
+    male: ["👨🏼", "👨", "👨🏻", "👨🏽", "👨🏾", "👨🏿"],
+    female: ["👩🏼", "👩", "👩🏻", "👩🏽", "👩🏾", "👩🏿"],
+  },
+  // YOUR CUSTOM NAMES GO HERE
+  names: {
+    male: [
+      "Achilles",
+      "Hector",
+      "Leonidas",
+      "Odysseus",
+      "Conan",
+      "Thor",
+      "Beowulf",
+      "Maximus",
+      "Lancelot",
+      "Galahad",
+      "Arthur",
+      "Ares",
+      "Vercingetorix",
+      "Genghis",
+      "Hannibal",
+    ],
+    female: [
+      "Artemisia",
+      "Boudica",
+      "Xena",
+      "Mulan",
+      "Joan",
+      "Lara",
+      "Freya",
+      "Atalanta",
+      "Cassandra",
+      "Nefertiti",
+      "Brienne",
+      "Eowyn",
+      "Morrigan",
+      "Medea",
+      "Tamar",
+    ],
+  },
+};
+
+class Helper {
+  static randomElement<T>(data: ReadonlyArray<T>): T | null {
+    if (data.length === 0) return null;
+    const randomPosition = Math.floor(Math.random() * data.length);
+    return data[randomPosition];
+  }
+
+  static randomBetweenRange(start: number, end: number): number {
+    return Math.floor(Math.random() * (end - start + 1)) + start;
+  }
+
+  static randomGenre() {
+    const randomGenre = Helper.randomElement(GENRES);
+    if (randomGenre === null)
+      throw new Error("Parece que los géneros no están bien definidos.");
+    return randomGenre;
+  }
+}
+
+class TournamentEmoji {
+  get attack(): string {
+    return "🗡️";
+  }
+  get speed(): string {
+    return "💨";
+  }
+  get defense(): string {
+    return "🛡️";
+  }
+  health(healthLevel: number): string {
+    return healthLevel > 50 ? "❤️" : healthLevel > 25 ? "❤️‍🩹" : "💔";
+  }
+  avatar(genre: Genre): string {
+    const randomAvatar = Helper.randomElement<string>(
+      DATA.avatars[genre]
+    ) as string;
+
+    return randomAvatar;
+  }
+}
+
+class Fighter {
+  constructor(
+    private health: number,
+    private avatar: string,
+    private name: string,
+    private velocity: number,
+    private attack: number,
+    private defense: number,
+    private tournamentEmojiService: TournamentEmoji
+  ) {}
+
+  get beautyName() {
+    return `[${this.avatar}${this.name}]`;
+  }
+
+  get beautyHealth() {
+    return `[${this.tournamentEmojiService.health(this.health)}${this.health}]`;
+  }
+
+  get beautyVelocity() {
+    return `[${this.tournamentEmojiService.speed}${this.velocity}]`;
+  }
+
+  get beautyAttack() {
+    return `[${this.tournamentEmojiService.attack}${this.attack}]`;
+  }
+
+  get beautyDefense() {
+    return `[${this.tournamentEmojiService.defense}${this.defense}]`;
+  }
+
+  toString(): string {
+    return `${this.beautyName}${this.beautyHealth}${this.beautyVelocity}${this.beautyAttack}${this.beautyDefense}`;
+  }
+}
+
+class FighterManager {
+  constructor(private tournamentEmojiService: TournamentEmoji) {}
+
+  createFighter(): Fighter {
+    const genre = Helper.randomGenre();
+
+    const fighterName = Helper.randomElement(DATA.names[genre]) as string;
+
+    const fighter = new Fighter(
+      100,
+      this.tournamentEmojiService.avatar(genre),
+      fighterName,
+      Helper.randomBetweenRange(1, 100),
+      Helper.randomBetweenRange(1, 100),
+      Helper.randomBetweenRange(1, 100),
+      this.tournamentEmojiService
+    );
+    return fighter;
+  }
+}
+
+// ========
+// = MAIN =
+// ========
+
+(() => {
+  const tournamentEmojiService = new TournamentEmoji();
+  const fighterManager = new FighterManager(tournamentEmojiService);
+
+  console.log(fighterManager.createFighter().toString());
+  console.log(fighterManager.createFighter().toString());
+  console.log(fighterManager.createFighter().toString());
+  console.log(fighterManager.createFighter().toString());
+})();
